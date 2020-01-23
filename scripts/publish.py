cebd18475990e2bc7445cdd12544a6590f9c521a
matsumotokoki <s17c1124br@s.chibakoudai.jp>
2020-01-23 18:44:08 +0900

    initial commit

---
+++ scripts/publish.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python2
+import rospy
+
+from sensor_msgs.msg import Image
+import numpy as np
+import cv2
+from cv_bridge import CvBridge
+import matplotlib.pyplot as plt
+
+rospy.init_node('send')
+pub = rospy.Publisher('send_img', Image, queue_size=1)
+rate = rospy.Rate(10)
+n = 0
+cap = cv2.VideoCapture(0)
+
+bridge = CvBridge()
+while not rospy.is_shutdown():
+    ret, frame = cap.read()
+    frame = cv2.resize(frame, (int(frame.shape[1]/4), int(frame.shape[0]/4)))
+    cv2.imshow('Raw Frame', frame)
+
+    k = cv2.waitKey(1)
+    if k == 27:
+        break
+    msg = bridge.cv2_to_imgmsg(frame, encoding="bgr8")
+    pub.publish(msg)
+    rate.sleep()
---
+++ scripts/subscribe.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import rospy
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+path = "./log.txt"
+image = []
+
+def cb(message):
+    bridge = CvBridge()
+    message = bridge.imgmsg_to_cv2(message)
+    rospy.loginfo(message)
+    print("-------------------\n")
+    plt.title('recived image')
+    plt.imshow(message[:,:,::-1])
+    plt.pause(.01)
+    plt.cla()
+
+if __name__ == '__main__':
+    rospy.init_node('recieve_img')
+    sub = rospy.Subscriber('send_img', Image, cb)
+    rospy.spin()
